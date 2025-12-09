fb07ef4c60a512ce48e4271bee05dc13bd611928
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-09 23:38:43 +0530

    Load and preprocess image

--- utils.py
+++ utils.py
@@ -9,4 +9,6 @@ def preprocess_custom_image(image_path):
     from PIL import Image
     import cv2
     
-    
+    img = Image.open(image_path).convert('L')
+    img = img.resize((28, 28))
+    img_array = np.array(img)
