c75d19463e0416fbd2312be71b5c34b8e82c2d0e
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-09 23:38:25 +0530

    Preprocess custom images for prediction

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ from tensorflow import keras
 def load_saved_model(model_path):
     return keras.models.load_model(model_path)
 
+def preprocess_custom_image(image_path):
+    from PIL import Image
+    import cv2
+    
+    
