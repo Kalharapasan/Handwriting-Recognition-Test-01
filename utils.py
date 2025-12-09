a26ca0cd23f4e3e45997b78a1d1277f9ad0e8fd8
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-09 23:34:43 +0530

    Load a saved model in utils.py

--- utils.py
+++ utils.py
@@ -2,3 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
+def load_saved_model(model_path):
+    """Load a saved model"""
+    return keras.models.load_model(model_path)
