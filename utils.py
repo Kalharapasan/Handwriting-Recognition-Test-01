fc2e03dcba04e6e325bafd92f1c936c1dc576dae
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-09 23:52:31 +0530

    Normalize

--- utils.py
+++ utils.py
@@ -15,3 +15,8 @@ def preprocess_custom_image(image_path):
     
     if np.mean(img_array) > 127:
         img_array = 255 - img_array
+    
+    img_array = img_array.astype('float32') / 255.0
+    img_array = img_array.reshape(1, 28, 28, 1)
+    
+    return img_array
